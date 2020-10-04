e006826d1f135b7ff8cab8fac4614a86198e1330
fahadshaikh123487@gmail.com <fahadshaikh123487@gmail.com>
2020-10-03 22:12:17 -0700

    change

--- src/Components/Contactus.js
+++ src/Components/Contactus.js
@@ -1,76 +1,144 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import imagethree from "../Assets/images/contact.png";
+import Fab from "@material-ui/core/Fab";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Axios from "axios";
+const nodemailer = require("nodemailer");
 
 const Contactus = () => {
+  const [email, setEmail] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const handleSubmit = () => {
+   const from = await Axios.post('/api/form',{
+    name,
+    email,
+    mobile,
+    message
+
+   }) 
+  };
+  useEffect(() => {
+    AOS.init({
+      duration: 3000,
+    });
+  }, []);
   return (
-    <div class="container" id="contact">
-      <div class="row mt-5 mb-5 col-lg-12">
-        <div class="card col-lg-6 col-md-6 col-12 ">
-          <div id="map-container-google-1" class="z-depth-1-half map-container">
-            <iframe
-              src="https://maps.google.com/maps?q=manhatan&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed"
-              frameborder="0"
-              style={{ border: 0 }}
-              width="500"
-              height="650"
-              allowfullscreen=""
-            ></iframe>
+    <div className="" id="contact">
+      <div className="row mt-5 mb-5 col-lg-12">
+        <div className="col-lg-6 col-md-6 col-12">
+          <div className="card" data-aos="fade-right">
+            <div
+              id="map-container-google-1"
+              className="z-depth-1-half map-container"
+            >
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15008.344640683788!2d75.3673927!3d19.8785834!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xcffa8235ad4d3d28!2sAdarsh%20Printers!5e0!3m2!1sen!2sin!4v1601675995224!5m2!1sen!2sin"
+                frameBorder="0"
+                style={{ border: 0 }}
+                width="620"
+                height="550"
+                allowfullscreen=""
+              ></iframe>
+            </div>
           </div>
         </div>
+        <div className="col-lg-6 col-md-6 col-12">
+          <div
+            className="card"
+            data-aos="fade-left"
+            style={{
+              backgroundImage: `url(${imagethree})`,
+              backgroundRepeat: "no-repeat",
+            }}
+          >
+            <div
+              className="mask"
+              style={{ backgroundColor: "rgba(88, 146, 114, 0.6)" }}
+            ></div>
+            <div className="card-body" style={{ zIndex: 1 }}>
+              <h4 className="card-title">
+                <div className="container mb-5">
+                  <h1
+                    className="text-center text-capitalize pt-5 "
+                    style={{ fontFamily: "Quicksand" }}
+                  >
+                    Get in touch
+                  </h1>
+                </div>
+              </h4>
+              <div className="w-50 mx-auto">
+                <form
+                  action="/action_page.php"
+                  style={{ fontFamily: "Quicksand" }}
+                >
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      id="defaultContactFormEmail"
+                      className="form-control mb-4"
+                      placeholder="Name"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="email"
+                      id="defaultContactFormEmail"
+                      className="form-control mb-4"
+                      placeholder="E-mail"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="number"
+                      id="defaultContactFormEmail"
+                      className="form-control mb-4"
+                      placeholder="Mobile"
+                      value={mobile}
+                      onChange={(e) => setMobile(e.target.value)}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <textarea
+                      className="form-control "
+                      placeholder="Enter Your Message"
+                      rows="6"
+                      value={message}
+                      onChange={(e) => setMessage(e.target.value)}
+                    ></textarea>
+                  </div>
 
-        <div class="card  col-lg-6 col-md-6 ">
-          <div class="card-body">
-            <h4 class="card-title">
-              <div class="container mb-5">
-                <h1 class="text-center text-capitalize pt-5 ">Contact Us</h1>
+                  <button
+                    type="submit"
+                    className="btn btn-primary"
+                    onClick={handleSubmit}
+                  >
+                    Submit
+                  </button>
+                </form>
               </div>
-            </h4>
-            <div class="w-50 mx-auto">
-              <form action="/action_page.php">
-                <div class="form-group">
-                  <label for="email">Your Name:</label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    placeholder="Enter Your Name"
-                    id="text"
-                  />
-                </div>
-                <div class="form-group">
-                  <label for="pwd">Email:</label>
-                  <input
-                    type="email"
-                    class="form-control"
-                    placeholder="Enter Your Email"
-                    id="email"
-                    autocomplete="offs"
-                  />
-                </div>
-                <div class="form-group">
-                  <label for="pwd">Mobile no:</label>
-                  <input
-                    type="number"
-                    class="form-control"
-                    placeholder="Enter Your Mobile no"
-                    id="email"
-                    autocomplete="offs"
-                  />
-                </div>
-                <div class="form-group">
-                  <label>Textarea</label>
-                  <textarea
-                    class="form-control "
-                    placeholder="Enter Your Textarea"
-                  ></textarea>
-                </div>
-
-                <button type="submit" class="btn btn-primary">
-                  Submit
-                </button>
-              </form>
             </div>
           </div>
         </div>
       </div>
+      <a
+        target="_blank"
+        className="btnWhatsapp"
+        href="https://api.whatsapp.com/send?phone=919561801015&text=Hi%20Aadarsh%20Printers,%20Help%20me%20get%20started!"
+      >
+        <Fab color="default" aria-label="add">
+          <i
+            className="fab fa-whatsapp"
+            style={{ fontSize: 24, color: "#25d366" }}
+          ></i>
+        </Fab>
+      </a>
     </div>
   );
 };
